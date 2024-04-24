69581070e9696885d8795cbb084ae5a31a8fafb6
Rain <gxl405@case.edu>
2024-04-24 02:36:28 -0400

    '完成api中url的修改以及setting的host修改'

--- generateFilter/urls.py
+++ generateFilter/urls.py
@@ -19,5 +19,5 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(('gaussianGenerate.urls', 'gaussianGenerate'), namespace='api')),
+    path('', include(('gaussianGenerate.urls', 'gaussianGenerate'), namespace='api')),
 ]
